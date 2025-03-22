4439ad74c1bfb8310fafa28363679e753fa6b1ec
Caleb Ganza <icaleb130@gmail.com>
2025-03-22 16:50:02 +0200

    Logging signatures

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -44,6 +44,9 @@ export async function POST(req: Request) {
     const secretHash = process.env.FLW_SECRET_HASH;
     const signature = req.headers.get("verif-hash");
 
+    console.log(signature);
+    console.log(secretHash);
+
     if (!signature || signature !== secretHash) {
       console.error("❌ Invalid webhook signature");
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
