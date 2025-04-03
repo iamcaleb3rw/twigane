865c33bb3a34b02634e7b68daafd07896dbd6033
Caleb Ganza <icaleb130@gmail.com>
2025-04-03 16:36:33 +0200

    Fixing type errors

--- components/LessonView.tsx
+++ components/LessonView.tsx
@@ -3,16 +3,10 @@ import YouTubePlayer from "./Video";
 import useCourseStore from "@/app/store/useCourseStore";
 import { Lesson } from "@/sanity.types";
 
-interface LessonViewProps {
-  initialLesson: any;
-}
-const LessonView = ({ initialLesson }: LessonViewProps) => {
+const LessonView = () => {
   const activeLesson = useCourseStore((state) => state.activeLesson);
   const setActiveLesson = useCourseStore((state) => state.setActiveLesson);
-  console.log(initialLesson);
-  if (!activeLesson) {
-    setActiveLesson(initialLesson);
-  }
+
   return (
     <div>
       <YouTubePlayer url={activeLesson?.videoUrl ?? ""} />
