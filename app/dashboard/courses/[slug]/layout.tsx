fa1b61f2dcdeeecad3ed9e0d0b09cbc09c3556ac
Caleb Ganza <icaleb130@gmail.com>
2025-04-08 19:38:29 +0200

    Removing unnecessary course fetching that could slow down the app

--- app/dashboard/courses/[slug]/layout.tsx
+++ app/dashboard/courses/[slug]/layout.tsx
@@ -1,9 +1,4 @@
-import { GetCourseBySlugQueryResult } from "@/sanity.types";
 import getCourseBySlug from "@/sanity/lib/courses/getCourseBySlug";
-// Your API fetching function
-
-// Import the existing type
-// Adjust path
 
 export default async function CourseLayout({
   children,
@@ -12,12 +7,6 @@ export default async function CourseLayout({
   children: React.ReactNode;
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
-  // Access Zustand's store method
-
-  // Fetch the course and make sure it matches the GetCourseBySlugQueryResult type
-  const course: GetCourseBySlugQueryResult = await getCourseBySlug(slug);
-
   // Update the Zustand store with the fetched course
 
   return <>{children}</>;
