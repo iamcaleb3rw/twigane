5358917cf170715a026ca74a5f47012b3121a23e
Caleb Ganza <icaleb130@gmail.com>
2025-04-18 18:48:23 +0200

    Debugging progress

--- components/sidebar-right.tsx
+++ components/sidebar-right.tsx
@@ -51,6 +51,7 @@ export function SidebarRight({
 
         setCourseData(courseInfo);
         setCourseProgress(progress);
+        alert(progress);
       } catch (error) {
         console.error("Error loading course data:", error);
       } finally {
